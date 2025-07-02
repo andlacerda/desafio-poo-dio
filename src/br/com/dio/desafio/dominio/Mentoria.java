402974562b48e2e474d600d7f8e87282042d49c6
Anderson Lacerda <andersonlacerdadev@gmail.com>
2025-07-01 22:29:18 -0300

    Vídeo sobre Abstração e Encapsulamento

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -2,16 +2,29 @@ package br.com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria extends Conteudo{
-
+public class Mentoria{
+    private String titulo;
+    private String descricao;
     private LocalDate data;
 
-    @Override
-    public double calcularXp() {
-        return XP_PADRAO + 20d;
+    public Mentoria(){
+
     }
 
-    public Mentoria() {
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
     public LocalDate getData() {
@@ -25,8 +38,8 @@ public class Mentoria extends Conteudo{
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
                 ", data=" + data +
                 '}';
     }
